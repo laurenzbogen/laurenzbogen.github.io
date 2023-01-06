037f07f4f79190fdfc1db0cab4ddc2c7a003a9d6
Laurenz Bogen <laurenzbogen@pm.me>
2023-01-06 03:45:29 +0100

    panning, dynamic grid size, text prompt

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+addEventListener('wheel', (event) => {
+    let sensitivity = 0.3
+    let delta = {
+        x: event.deltaX * sensitivity,
+        y: event.deltaY * sensitivity
+    }
+
+    compute_delta(delta)
+
+    last_percentage = next_percentage
+});
+
+
+
+
+
+
+
+
+
+
 let grid = document.querySelector(".image_grid")
 let images = Array.from(document.querySelectorAll(".image"))
-let templates = Array.from(document.querySelector(".templates").children)
-shuffle(templates)
-
-templates.forEach(element =>{
-    element.classList.add("tile")
-    grid.appendChild(element)
-})
 
 let tiles = Array.from(grid.querySelectorAll(".tile"))
+let columns = Math.ceil(Math.sqrt(tiles.length))
+
+grid.style.setProperty("--columns", columns)
+
+shuffle(tiles)
+tiles.forEach(tile =>{
+    grid.removeChild(tile)
+    grid.appendChild(tile)
+})
+
 let videos = Array.from(grid.querySelectorAll("video"))
 
 videos.forEach(video =>{
@@ -33,7 +57,6 @@ tiles.forEach((tile, i) =>{
 })
 
 const trigger_stagger_animation_over = (index)=>{
-    console.log(index)
     anime({
         targets: '.tile',
         easing: "linear",
@@ -75,33 +98,39 @@ window.onmousedown = e =>{
     }
 
     window.onmousemove = e =>{
+        let sensitivity = 0.85
         const mouseDelta = {
-            x: mouse_down_at.x - e.clientX,
-            y: mouse_down_at.y - e.clientY
+            x: (mouse_down_at.x - e.clientX) * sensitivity,
+            y: (mouse_down_at.y - e.clientY) * sensitivity
         }
-        const maxDelta = {
-            x: window.innerWidth / 2,
-            y: window.innerHeight / 2
-        }
-        const percentage = {
-            x: mouseDelta.x / maxDelta.x * -100,
-            y: mouseDelta.y / maxDelta.y * -100
-        }
-        next_percentage = {
-            x: Math.min(2, Math.max(-50, last_percentage.x + percentage.x)),
-            y: Math.min(1, Math.max(-65, last_percentage.y + percentage.y))
-        }
-    
-        grid.animate({
-            transform: `translate(${next_percentage.x}%, ${next_percentage.y}%)`
-        }, {duration: 1200, fill:"forwards"})
 
-        images.forEach(image =>{
-            image.animate({
-                objectPosition: `${100 + next_percentage.x}% ${60 + next_percentage.y/5}%`
-            }, {duration: 1200, fill:"forwards"})
-        })
+        compute_delta(mouseDelta)
     }
+}
+
+const compute_delta = (delta)=>{
+    const maxDelta = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+    }
+    const percentage = {
+        x: delta.x / maxDelta.x * -100,
+        y: delta.y / maxDelta.y * -100
+    }
+    next_percentage = {
+        x: Math.min(2, Math.max(-50, last_percentage.x + percentage.x)),
+        y: Math.min(1, Math.max(-65, last_percentage.y + percentage.y))
+    }
+
+    grid.animate({
+        transform: `translate(${next_percentage.x}%, ${next_percentage.y}%)`
+    }, {duration: 1200, fill:"forwards"})
+
+    images.forEach(image =>{
+        image.animate({
+            objectPosition: `${100 + next_percentage.x}% ${60 + next_percentage.y/5}%`
+        }, {duration: 1200, fill:"forwards"})
+    })
 }
 
 window.onmouseup = e =>{
